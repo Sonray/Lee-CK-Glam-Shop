2398bf571c86e52d8e5452c760e77ad7ad2295c7
David Okwacha <osweredavid@gmail.com>
2021-03-25 02:22:34 +0300

    Started on Items app by initialising the Product model

--- Items/models.py
+++ Items/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from  Authentication.models import  Account
+from django.db.models import CharField
 
 # Create your models here.
+
+class  Product(models.Model):
+    pass
