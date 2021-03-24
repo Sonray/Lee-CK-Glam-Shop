d683fec748974161d95cd61d0e60e003c170b9ad
David Okwacha <osweredavid@gmail.com>
2021-03-25 02:48:28 +0300

    Added attributes to the product details

--- Items/models.py
+++ Items/models.py
@@ -4,5 +4,17 @@ from django.db.models import CharField
 
 # Create your models here.
 
-class  Product(models.Model):
-    pass
+class  Product_details(models.Model):
+    product_image       = models.ImageField( blank=False)
+    product_name        = models.CharField( blank=False)
+    category            = models.CharField( blank=False)
+    sub_category        = models.CharField( blank=False)
+    admin_id            = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="product_upload")
+    date_uploaded       = models.DateTimeField(auto_now_add = True, null =True)
+    
+    old_price           = models.IntegerField()
+    new_price           = models.IntegerField()
+    product_description = models.CharField()
+    
+
+    
