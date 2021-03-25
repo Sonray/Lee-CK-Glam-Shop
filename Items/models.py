3c48b89f17d35c54c3f23eab632554279231aa76
David Okwacha <osweredavid@gmail.com>
2021-03-26 02:57:37 +0300

    Added product review and specifications and their product connection

--- Items/models.py
+++ Items/models.py
@@ -4,17 +4,35 @@ from django.db.models import CharField
 
 # Create your models here.
 
+class Key_features(models.Model):
+    product_feature     = models.TextField()
+    
+    def __str__(self):
+        return self.product_feature
+    
+    
+class  Product_specifications(models.Model):
+    product_specification   = models.TextField()
+    
+    def __str__(self):
+        return self.product_specification
+
 class  Product_details(models.Model):
     product_image       = models.ImageField( blank=False)
     product_name        = models.CharField( blank=False)
     category            = models.CharField( blank=False)
     sub_category        = models.CharField( blank=False)
-    admin_id            = models.ForeignKey( Account, on_delete=models.CASCADE, related_name="product_upload_admin")
+    admin_id            = models.ForeignKey( Account, on_delete=models.SET_NULL, blank= True , null=True , related_name="product_upload_admin")
     date_uploaded       = models.DateTimeField( auto_now_add = True, null =True)
     
     old_price           = models.IntegerField()
     new_price           = models.IntegerField()
     product_description = models.CharField()
+    key_features        = models.ManyToManyField(key_features, blank=True)
+    product_specifications = models.ManyToManyField(product_description, blank=True)
+    
+    def __str__(self):
+        return self.product_name + ' ' + self.category
     
     def save_product(self):
         self.save()
@@ -25,8 +43,8 @@ class  Product_details(models.Model):
     
     
 class Reviews(models.Model):
-    product             = models.ForeignKey( Product_details,on_delete = models.CASCADE, related_name="Product_review")
-    user_id             = models.ForeignKey( Account, on_delete=models.CASCADE, related_name="Product_review_user")
+    product             = models.ForeignKey( Product_details,on_delete = models.CASCADE , related_name="Product_review")
+    user_id             = models.ForeignKey( Account, on_delete=models.SET_NULL, blank= True , null=True , related_name="Product_review_user")
     date_reviewed       = models.DateTimeField( auto_now_add = True, null =True)
     Product_review      = models.TextField( blank=False)
     product_rating      = models.IntegerField( blank=False)
@@ -37,3 +55,6 @@ class Reviews(models.Model):
     def delete_review(self):
         self.delete()
 
+
+class  Order(models.Model):
+    pass
