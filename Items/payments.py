74b0f0d7ae44642e7254fe63c9eea97912ee0486
David Okwacha <osweredavid@gmail.com>
2021-04-28 14:35:21 +0300

    generated access token for lipa na mpesa

--- Items/payments.py
+++ Items/payments.py
@@ -2,6 +2,7 @@ import requests
 from  decouple import config
 from  datetime import datetime
 import base64
+from requests.auth import HTTPBasicAuth
 
 the_time = datetime.now()
 formatted_time = the_time.strftime('%Y%m%d%H%M%S')
@@ -9,10 +10,18 @@ formatted_time = the_time.strftime('%Y%m%d%H%M%S')
 data_to_encode = config('Lipa_Na_Mpesa_Shortcode') + config('Lipa_Na_Mpesa_Passkey')+ formatted_time
 encoded_string = base64.b64encode(data_to_encode.encode())
 decode_password = encoded_string.decode('utf-8')
+  
+consumer_key = config ('Consumer_Key')
+consumer_secret = config ('Consumer_Secret')
+api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
+r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+
+json_response = r.json()
+access_token1 = json_response['access_token']
   
 def  Lipa_na_mpesa(self, phone_number, amount):
-    access_token = "Access-Token"
+    access_token = access_token1
     api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     headers = { "Authorization": "Bearer %s" % access_token }
     request = {
